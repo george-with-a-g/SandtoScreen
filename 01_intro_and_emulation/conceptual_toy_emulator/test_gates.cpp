0922f8403d8940fb30385217d0934fef203a84ae
george-with-a-g <apsgeorge6@gmail.com>
2026-08-20 16:59:35 +0300

    Testing a NAND gate.

--- 01_intro_and_emulation/conceptual_toy_emulator/test_gates.cpp
+++ 01_intro_and_emulation/conceptual_toy_emulator/test_gates.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "toy_emulator.hpp"
 
-int main() {
+void testing_not_gate(){
     // 1. TESTING A NOT GATE
     std::cout << ".....TESTING THE NOT GATE....." << std::endl;
     std::cout << "Giving input for NOT gate as HIGH" << std::endl;
@@ -10,18 +10,29 @@ int main() {
     std::cout << "Changing input for NOT gate to LOW" << std::endl;
     LogicLevel not_out_alternate = cmos_not(LOW); // Will return HIGH
     std::cout << "NOT GATE: " << (not_out_alternate == HIGH ? "5V (Success)" : "Fail") << "\n";
+}
 
-    std::cout << ".....TESTING THE AND GATE....." << std::endl;
-    /*
-    // 2. Test CMOS NAND Gate
-    LogicLevel nand_out = cmos_nand(HIGH, HIGH);
-    std::cout << "NAND(1, 1): " << nand_out << " (Expected: 0)\n";
+void testing_nand_gate(){
+    // 2. TESTING AN AND  GATE
+    std::cout << ".....TESTING THE NAND GATE....." << std::endl;
+    // Case 1: (0, 0) -> HIGH (5V)
+    LogicLevel nand_out00 = cmos_nand(LOW, LOW);
+    std::cout << "NAND(0, 0): " << (nand_out00 == HIGH ? "5V (Success)" : "Fail") << "\n";
+    // Case 2: (0, 1) -> HIGH (5V)
+    LogicLevel nand_out01 = cmos_nand(LOW, HIGH);
+    std::cout << "NAND(0, 1): " << (nand_out01 == HIGH ? "5V (Success)" : "Fail") << "\n";
+    // Case 3: (1, 0) -> HIGH (5V)
+    LogicLevel nand_out10 = cmos_nand(HIGH, LOW);
+    std::cout << "NAND(1, 0): " << (nand_out10 == HIGH ? "5V (Success)" : "Fail") << "\n";
+    // Case 4: (1, 1) -> LOW (0V) [Both series NMOS turn ON!]
+    LogicLevel nand_out11 = cmos_nand(HIGH, HIGH);
+    std::cout << "NAND(1, 1): " << (nand_out11 == LOW ? "0V (Success)" : "Fail") << "\n";
+}
 
-    // 3. Test Half Adder
-    AdderResult res = half_adder(HIGH, HIGH);
-    std::cout << "Half Adder 1 + 1 -> Sum: " << res.sum 
-              << ", Carry: " << res.carry_out << " (Expected: Sum 0, Carry 1)\n";
-    */
 
+int main() {
+
+    testing_not_gate();
+    //testing_nand_gate();
     return 0;
 }
